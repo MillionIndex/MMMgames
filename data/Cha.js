e73eb4a02bb9e030373ae7644e6b770617098a43
Multi Trillion <synk8824@gmail.com>
2026-08-17 21:33:27 +0900

    Update Cha.js

--- data/Cha.js
+++ data/Cha.js
@@ -1,13 +1,12 @@
-// data/Cha.js
-// 味方キャラクターのデータベース
+// 味方キャラクターデータベース
 const playerUnitDb = [
-  { id: 0, name: "Data1", cost: 100, hp: 100, atk: 10, speed: 1.5, range: 25, color: '#2ecc71' },
-  { id: 1, name: "Data2",   cost: 250, hp: 250, atk: 25, speed: 1.0, range: 30, color: '#3498db' },
-  { id: 2, name: "Data3",   cost: 500, hp: 600, atk: 50, speed: 0.7, range: 40, color: '#9b59b6' }
+  { id: 0, name: "わんこ", cost: 100, hp: 100, atk: 30, speed: 1.5, range: 25, color: '#2ecc71', atkInterval: 30 },
+  { id: 1, name: "くま",   cost: 250, hp: 250, atk: 80, speed: 1.0, range: 30, color: '#3498db', atkInterval: 45 },
+  { id: 2, name: "ぞう",   cost: 500, hp: 600, atk: 200, speed: 0.7, range: 40, color: '#9b59b6', atkInterval: 60 }
 ];
 
-// 敵キャラクターのデータベース
+// 敵キャラクターデータベース
 const enemyUnitDb = {
-  basic:  { id: 'e0', name: "Mob1", hp: 60,  atk: 6,  speed: 1.0, range: 25, color: '#e74c3c' },
-  strong: { id: 'e1', name: "Mob2", hp: 350, atk: 22, speed: 0.8, range: 30, color: '#c0392b' }
+  basic:  { id: 'e0', name: "敵わんこ", hp: 60,  atk: 15, speed: 1.0, range: 25, color: '#e74c3c', atkInterval: 30 },
+  strong: { id: 'e1', name: "敵ゴリラ", hp: 350, atk: 60, speed: 0.8, range: 30, color: '#c0392b', atkInterval: 50 }
 };
